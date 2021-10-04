e2cf6343e274ee28e910baff7e5d6da61828d3d9
long ho <long_ho@datahouse.com>
2021-10-04 15:40:27 +0700

    Finish login route - 04.10.2021

--- src/models/user.js
+++ src/models/user.js
@@ -76,6 +76,22 @@ userSchema.methods.toJSON = function () {
   return userObject;
 };
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login!');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
+
 userSchema.statics.verifyUser = async (id, verifyCode) => {
   const user = await User.findById(id);
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,6 +4,19 @@ const router = new express.Router();
 const { sendMail } = require('../utils/mailer');
 const { generateVerifyCode } = require('../utils/generateVerifyCode');
 
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    let token = null;
+    if (user.isVerified) {
+      token = await user.generateAuthToken();
+    }
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
   const verifyCode = generateVerifyCode();
