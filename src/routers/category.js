369a3c4963a4da552fe46f72852ac41d2e195b3c
long ho <long_ho@datahouse.com>
2021-11-12 15:27:50 +0700

    Replace image of category to Cloudinary instead DB - 12.11.2021

--- src/routers/category.js
+++ src/routers/category.js
@@ -1,32 +1,13 @@
 const express = require('express');
-const multer = require('multer');
-const sharp = require('sharp');
 const Category = require('../models/category');
-const { base64ArrayBuffer } = require('../utils/convertArrayBufferToBase64String');
 const router = new express.Router();
 const auth = require('../middleware/auth');
-
-const upload = multer({
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(new Error('Please upload an image'));
-    }
-    cb(undefined, true);
-  }
-});
 
 /* 
   Admin route, Add auth middleware later
 */
-router.post('/categories', upload.single('categoryImage'), async (req, res) => {
+router.post('/categories', async (req, res) => {
   const category = new Category(req.body);
-  const buffer = await sharp(req.file.buffer).resize({ width: 150, height: 150 }).png().toBuffer();
-  const base64String = base64ArrayBuffer(buffer);
-  category.image = base64String;
-
   try {
     await category.save();
     res.status(201).send({ category });
