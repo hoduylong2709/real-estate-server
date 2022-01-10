3332e88fef27f8303801299339c3552a3d6fe99d
long ho <long_ho@datahouse.com>
2022-01-10 10:19:20 +0700

    Implement SocketIO, handle emit and receive message events - 10.01.2022

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const http = require('http');
 require('./db/mongoose');
 const userRouter = require('./routers/user');
 const categoryRouter = require('./routers/category');
@@ -18,6 +19,57 @@ app.use(ratingRouter);
 app.use(conversationRouter);
 app.use(messageRouter);
 
-app.listen(port, () => {
+const server = http.createServer(app);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*'
+  }
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some(user => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = socketId => {
+  users = users.filter(user => user.socketId !== socketId);
+};
+
+const getUser = userId => {
+  return users.find(user => user.userId === userId);
+};
+
+io.on('connection', socket => {
+  console.log('A user connected.');
+
+  // Take userId and socketId from user connected
+  socket.on('addUser', userId => {
+    addUser(userId, socket.id);
+  });
+
+  socket.on('getUsers', () => {
+    io.emit('getUsers', users);
+  });
+
+  // Send and get message
+  socket.on('sendMessage', message => {
+    console.log(message);
+    const user = getUser(message.receiverId);
+    console.log(user);
+    io.to(user.socketId).emit('getMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected.');
+    removeUser(socket.id);
+    console.log('users', users);
+    io.emit('getUsers', users);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
